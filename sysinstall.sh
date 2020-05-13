53d26eb39cd9be230689c1f77808e5c07459faae
BitTim <tim.anhalt.2004@protonmail.com>
2020-05-13 14:05:06 +0200

    Added powerline

--- sysinstall.sh
+++ sysinstall.sh
@@ -3,7 +3,7 @@
 echo Updating packagelist
 sudo pacman -Syu
 echo Installing programs
-sudo pacman -S steam discord go sdl2 sdl2_ttf sdl2_image sdl2_mixer nvidia lutris gimp vim neofetch
+sudo pacman -S steam discord go sdl2 sdl2_ttf sdl2_image sdl2_mixer nvidia lutris gimp vim neofetch powerline
 echo Installing \"yay\"
 git clone https://aur.archlinux.org/yay.git
 cd yay
